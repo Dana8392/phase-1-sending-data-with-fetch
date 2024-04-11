ceee0cfaba8f031e3ebae43718c92827d510c709
Dana Maraqa <dana.maraqa@hotmail.com>
2024-04-11 21:59:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email){
+    const formData = {
+        name:name,
+        email:email
+    };
+
+    const configurationObject={
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+
+        body:JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(Object=>{
+        document.body.innerHTML+=Object.id;
+    })
+
+    .catch(error=>{
+        document.body.innerHTML+=error.message;
+    })
+
+}
